e4e1f9438f05e72fb007bff25001fc32d53adcb1
kelvinmurimi <murimikelvin229@gmail.com>
2023-05-31 09:34:10 +0300

    added chatbot script

--- layouts/header.php
+++ layouts/header.php
@@ -30,7 +30,19 @@ require('includes/variables.php');
 </head>
 
 <body id="top">
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6476ea10ad80445890f01e0f/1h1o84l2t';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
   <!-- 
     - #HEADER
   -->
